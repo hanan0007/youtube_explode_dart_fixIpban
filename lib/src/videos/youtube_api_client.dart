689c790bbd6bf5a5c5acc224f136643f7292753e
Muhammad Hanan <147533694+hanan0007@users.noreply.github.com>
2025-06-18 18:57:26 +0500

    Update youtube_api_client.dart fixes

--- lib/src/videos/youtube_api_client.dart
+++ lib/src/videos/youtube_api_client.dart
@@ -19,14 +19,15 @@ class YoutubeApiClient {
 
   // from https://github.com/yt-dlp/yt-dlp/blob/7794374de8afb20499b023107e2abfd4e6b93ee4/yt_dlp/extractor/youtube/_base.py#L136
   /// Has limited streams but doesn't require signature deciphering.
-  YoutubeApiClient randomIOSClient() {
+ 
+static final ios = (() {
   final random = Random();
   final major = 20;
   final minor = random.nextInt(30) + 1;
   final patch = random.nextInt(10);
-
   final version = '$major.$minor.$patch';
   final deviceModel = 'iPhone${14 + random.nextInt(4)},${random.nextInt(4) + 1}';
+  final osVersion = '18.${random.nextInt(5)}.${random.nextInt(99)}';
 
   return YoutubeApiClient({
     'context': {
@@ -40,16 +41,22 @@ class YoutubeApiClient {
         'hl': 'en',
         "platform": "MOBILE",
         'osName': 'IOS',
-        'osVersion': '18.${random.nextInt(5)}.${random.nextInt(99)}',
+        'osVersion': osVersion,
         'timeZone': 'UTC',
         'gl': 'US',
         'utcOffsetMinutes': 0
       }
     },
   }, 'https://www.youtube.com/youtubei/v1/player?key=AIzaSyB-63vPrdThhKuerbB2N_l7Kwwcxj6yUAc&prettyPrint=false', headers: {
-    'Cookie': "LOGIN_INFO=AFmmF2swRQIgKGP9Qc4c_JSBr3JDW03IIK_lb6FimOyIYtxUJmKYIgECIQCQYge5iBtxNg_YJlJ5kcEGUGjgVh-KKtfnlZXAcBOQZg:QUQ3MjNmeEktdWJSdFlZV2hTUEdQOG9iSENZZ3hPXy14MUNHaDF2c1NRc1JDSy1Kd2FGZVZ2MTVqZjdDaFg4TExjc2pGSjJDUkdkRk1NU2FWR1RyUUNOVUFsaEtRcGFDcHdmeE5iQV8zSl9wcTI5eFJQczNhQzlLMklGX05BR3UzWXNmUnU3TnNCTGN5N1Y0Q09yX2lYM2tRZWhHYjZlb0lR; VISITOR_INFO1_LIVE=b9gKi5jBfiU; VISITOR_PRIVACY_METADATA=CgJQSxIEGgAgOQ%3D%3D; SID=g.a000xQiM13KDXa44t7rv7KDxcyAQIaEzLEf1Z5iXyXaWPa_E0KfqPBpF20mfLremKLHl_p_7QgACgYKAacSARQSFQHGX2MibZ2Q3ycMyKOrIlQ8QZviBRoVAUF8yKrhSBA_c0zGilB-SFuriPQD0076; __Secure-1PSID=g.a000xQiM13KDXa44t7rv7KDxcyAQIaEzLEf1Z5iXyXaWPa_E0KfqSlt1J3DvzfI7FCmQxosctwACgYKAbsSARQSFQHGX2MiuQt5RUgBosblK-W-kzR3XRoVAUF8yKp_gLIMT60q_hQk05c4IF0a0076; __Secure-3PSID=g.a000xQiM13KDXa44t7rv7KDxcyAQIaEzLEf1Z5iXyXaWPa_E0KfqJbOnrZibqKc10znchZTmBAACgYKAcgSARQSFQHGX2MiFZIkDt6-Q3FKGnvCzheB7RoVAUF8yKprPeqXHK6kLjjajL8buhX30076; HSID=AQRM7kfT0klVdejUu; SSID=AyrtsnJlL5VtpEQw8; APISID=eWBITK-BqGLFzwt8/Ak1utx1_IzgdAg8wN; SAPISID=C85_wOpQCTSSG8Fm/AW-I46I53dShBh9vH; __Secure-1PAPISID=C85_wOpQCTSSG8Fm/AW-I46I53dShBh9vH; __Secure-3PAPISID=C85_wOpQCTSSG8Fm/AW-I46I53dShBh9vH; YSC=ffi8T050DMc; __Secure-ROLLOUT_TOKEN=CMeO85Oy8bLb4gEQ39PQrofFjAMY4aC6h7H6jQM%3D; PREF=f6=40000000&tz=Asia.Karachi&f5=30000&f7=100&f4=4000000; __Secure-1PSIDTS=sidts-CjEB5H03P6tAz7tnCKaFBNVgABwfhuZySYyhZXBEO0Cf7nXGbbAuRule_eiOfz4b9Qx3EAA; __Secure-3PSIDTS=sidts-CjEB5H03P6tAz7tnCKaFBNVgABwfhuZySYyhZXBEO0Cf7nXGbbAuRule_eiOfz4b9Qx3EAA; SIDCC=AKEyXzWRCEV69aK0KLzMAmtzZfiWnparhRDa26UigZAW4NWpFJgFkDiX6-AQOx6qYE6jh_z39JU; __Secure-1PSIDCC=AKEyXzUYB7s1o-EogypAZjot4bre5HGtVzEW2pWmgOEBrVfTCwYB9M1uKcPn3_7xDeDV0GcpSUQ; __Secure-3PSIDCC=AKEyXzUamjhQdEU-P9eQb_0_cxPGUKYf2Pn4IdsNyQnKkKcJEJaAifzkEz6hhPCT2Rik3MEzp0A",
+    'Content-Type': 'application/json',
+    'Origin': 'https://www.youtube.com',
+    'User-Agent':
+        'com.google.ios.youtube/$version ($deviceModel; U; CPU iOS 18_3_2 like Mac OS X;)',
+
+    // Insert your full, valid YouTube cookies here:
+    'Cookie': 'LOGIN_INFO=AFmmF2swRQIgKGP9Qc4c_JSBr3JDW03IIK_lb6FimOyIYtxUJmKYIgECIQCQYge5iBtxNg_YJlJ5kcEGUGjgVh-KKtfnlZXAcBOQZg:QUQ3MjNmeEktdWJSdFlZV2hTUEdQOG9iSENZZ3hPXy14MUNHaDF2c1NRc1JDSy1Kd2FGZVZ2MTVqZjdDaFg4TExjc2pGSjJDUkdkRk1NU2FWR1RyUUNOVUFsaEtRcGFDcHdmeE5iQV8zSl9wcTI5eFJQczNhQzlLMklGX05BR3UzWXNmUnU3TnNCTGN5N1Y0Q09yX2lYM2tRZWhHYjZlb0lR; VISITOR_INFO1_LIVE=b9gKi5jBfiU; VISITOR_PRIVACY_METADATA=CgJQSxIEGgAgOQ%3D%3D; SID=g.a000xQiM13KDXa44t7rv7KDxcyAQIaEzLEf1Z5iXyXaWPa_E0KfqPBpF20mfLremKLHl_p_7QgACgYKAacSARQSFQHGX2MibZ2Q3ycMyKOrIlQ8QZviBRoVAUF8yKrhSBA_c0zGilB-SFuriPQD0076; __Secure-1PSID=g.a000xQiM13KDXa44t7rv7KDxcyAQIaEzLEf1Z5iXyXaWPa_E0KfqSlt1J3DvzfI7FCmQxosctwACgYKAbsSARQSFQHGX2MiuQt5RUgBosblK-W-kzR3XRoVAUF8yKp_gLIMT60q_hQk05c4IF0a0076; __Secure-3PSID=g.a000xQiM13KDXa44t7rv7KDxcyAQIaEzLEf1Z5iXyXaWPa_E0KfqJbOnrZibqKc10znchZTmBAACgYKAcgSARQSFQHGX2MiFZIkDt6-Q3FKGnvCzheB7RoVAUF8yKprPeqXHK6kLjjajL8buhX30076; HSID=AQRM7kfT0klVdejUu; SSID=AyrtsnJlL5VtpEQw8; APISID=eWBITK-BqGLFzwt8/Ak1utx1_IzgdAg8wN; SAPISID=C85_wOpQCTSSG8Fm/AW-I46I53dShBh9vH; __Secure-1PAPISID=C85_wOpQCTSSG8Fm/AW-I46I53dShBh9vH; __Secure-3PAPISID=C85_wOpQCTSSG8Fm/AW-I46I53dShBh9vH; YSC=ffi8T050DMc; __Secure-ROLLOUT_TOKEN=CMeO85Oy8bLb4gEQ39PQrofFjAMY4aC6h7H6jQM%3D; PREF=f6=40000000&tz=Asia.Karachi&f5=30000&f7=100&f4=4000000; __Secure-1PSIDTS=sidts-CjEB5H03P6tAz7tnCKaFBNVgABwfhuZySYyhZXBEO0Cf7nXGbbAuRule_eiOfz4b9Qx3EAA; __Secure-3PSIDTS=sidts-CjEB5H03P6tAz7tnCKaFBNVgABwfhuZySYyhZXBEO0Cf7nXGbbAuRule_eiOfz4b9Qx3EAA; SIDCC=AKEyXzWRCEV69aK0KLzMAmtzZfiWnparhRDa26UigZAW4NWpFJgFkDiX6-AQOx6qYE6jh_z39JU; __Secure-1PSIDCC=AKEyXzUYB7s1o-EogypAZjot4bre5HGtVzEW2pWmgOEBrVfTCwYB9M1uKcPn3_7xDeDV0GcpSUQ; __Secure-3PSIDCC=AKEyXzUamjhQdEU-P9eQb_0_cxPGUKYf2Pn4IdsNyQnKkKcJEJaAifzkEz6hhPCT2Rik3MEzp0A'
   });
-}
+})();
 
 
   /// This provides also muxed streams but seems less reliable than [ios].
